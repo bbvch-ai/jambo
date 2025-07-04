59106c77f7836b6f748f2074a4bff28bf5adaae0
Thomas Mannhart <thomas.mannhart@bbv.ch>
2025-07-04 11:53:39 +0200

    fix(jambo): skip Annotated wrapping for trivial anyOf field constraints

--- jambo/parser/anyof_type_parser.py
+++ jambo/parser/anyof_type_parser.py
@@ -34,8 +34,34 @@ class AnyOfTypeParser(GenericTypeParser):
         # By defining the type as Union of Annotated type we can use the Field validator
         # to enforce the constraints of each union type when needed.
         # We use Annotated to attach the Field validators to the type.
+        # Only wrap in Annotated[T, Field(**v)] if there are meaningful field constraints
+        # Don't wrap for simple cases where v only contains {'default': None}
         field_types = [
-            Annotated[t, Field(**v)] if v is not None else t for t, v in sub_types
+            Annotated[t, Field(**v)] if self._has_meaningful_constraints(v) else t
+            for t, v in sub_types
         ]
 
         return Union[(*field_types,)], mapped_properties
+
+    @staticmethod
+    def _has_meaningful_constraints(field_props):
+        """
+        Check if field properties contain meaningful constraints that require Field wrapping.
+
+        Returns False if:
+        - field_props is None or empty
+        - field_props only contains {'default': None}
+
+        Returns True if:
+        - field_props contains a non-None default value
+        - field_props contains other constraint properties (min_length, max_length, pattern, etc.)
+        """
+        if not field_props:
+            return False
+
+        # If only default is set and it's None, no meaningful constraints
+        if len(field_props) == 1 and field_props.get('default') is None:
+            return False
+
+        # If there are multiple properties or non-None default, that's meaningful
+        return True
